d296d65fb4d85da83fb0caa95224b1c604071a84
Paul <cardenas.sebastian91@gmail.com>
2024-07-18 13:04:57 +0200

    Adding missing files

---
+++ src/test/java/com/personal/bookrecommendations/BookrecommendationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.bookrecommendations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrecommendationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
